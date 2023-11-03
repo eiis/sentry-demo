91ed18372610175b443505f034092787a6fbb82b
zhangdi <1258956799@qq.com>
2023-11-03 19:13:15 +0800

    chore:add sentry

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+function sendSentryData() {
+   throw new Error('Sentry error')
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button @click="sendSentryData">Click me</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
